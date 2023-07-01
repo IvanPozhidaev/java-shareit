a7224d381726db0250038e24d347c0783898f66d
Ivan Pozhidaev <craig1408@yandex.ru>
2023-07-01 14:41:05 +0700

    fix: убрать ненужные константы

--- src/main/java/ru/practicum/shareit/item/Comment.java
+++ src/main/java/ru/practicum/shareit/item/Comment.java
@@ -16,22 +16,17 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Entity(name = "comments")
 public class Comment {
-    public static final int MAX_TEXT_LENGTH = 512;
-    public static final String TEXT_COLUMN_NAME = "text";
-    public static final String ITEM_COLUMN_NAME = "item";
-    public static final String OWNER_COLUMN_NAME = "owner";
-    public static final String ID_COLUMN_NAME = "comment_id";
 
     @Id
-    @Column(name = ID_COLUMN_NAME)
+    @Column(name = "comment_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = TEXT_COLUMN_NAME, nullable = false, length = MAX_TEXT_LENGTH)
+    @Column(name = "text", nullable = false, length = 512)
     private String text;
 
     @ManyToOne
-    @JoinColumn(name = Item.ID_COLUMN_NAME)
+    @JoinColumn(name = "item_id")
     private Item item;
 
     @ManyToOne
--- src/main/java/ru/practicum/shareit/item/Item.java
+++ src/main/java/ru/practicum/shareit/item/Item.java
@@ -13,27 +13,21 @@ import javax.persistence.*;
 @AllArgsConstructor
 @Entity(name = "items")
 public class Item {
-    public static final int MAX_DESCRIPTION_LENGTH = 512;
-    public static final String NAME_COLUMN_NAME = "name";
-    public static final String ID_COLUMN_NAME = "item_id";
-    public static final String OWNER_COLUMN_NAME = "owner";
-    public static final String AVAILABLE_COLUMN_NAME = "available";
-    public static final String DESCRIPTION_COLUMN_NAME = "description";
 
     @Id
-    @Column(name = ID_COLUMN_NAME)
+    @Column(name = "item_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = NAME_COLUMN_NAME, nullable = false)
+    @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name = DESCRIPTION_COLUMN_NAME, nullable = false, length = MAX_DESCRIPTION_LENGTH)
+    @Column(name = "description", nullable = false, length = 512)
     private String description;
 
-    @Column(name = AVAILABLE_COLUMN_NAME, nullable = false)
+    @Column(name = "available", nullable = false)
     private Boolean available;
 
-    @Column(name = OWNER_COLUMN_NAME, nullable = false)
+    @Column(name = "owner", nullable = false)
     private Long owner;
 }
