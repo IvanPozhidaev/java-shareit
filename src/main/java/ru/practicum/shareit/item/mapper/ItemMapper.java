08c8c66e2e4821fcc8d43c095a718626d2896b8a
Ivan Pozhidaev <craig1408@yandex.ru>
2023-07-06 19:01:54 +0700

    fix: fix

--- src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -21,6 +21,15 @@ public class ItemMapper {
         return dto;
     }
 
+    public static ItemDto toDto(Item item) {
+        ItemDto dto = new ItemDto();
+        dto.setId(item.getId());
+        dto.setName(item.getName());
+        dto.setDescription(item.getDescription());
+        dto.setAvailable(item.getAvailable());
+        return dto;
+    }
+
     public static ItemDto toDto(Item item,
                                 Booking lastBooking,
                                 Booking nextBooking,
