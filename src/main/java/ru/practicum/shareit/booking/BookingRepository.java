1be72dd64ccfa17579d4f945c1e92dc8ab55d6fc
Ivan Pozhidaev <craig1408@yandex.ru>
2023-07-11 15:58:14 +0700

    fix:checkstyle

--- src/main/java/ru/practicum/shareit/booking/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/BookingRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import ru.practicum.shareit.item.Item;
 
 import java.time.LocalDateTime;
 import java.util.List;
