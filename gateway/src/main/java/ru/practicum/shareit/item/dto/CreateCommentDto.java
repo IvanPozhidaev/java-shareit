cf347a4a6642d6713bb16f2f56ae1b58d15f37dd
Ivan Pozhidaev <craig1408@yandex.ru>
2023-07-24 09:38:11 +0700

    fix: добавитоь валидацию в CreateCommentDto.class

--- gateway/src/main/java/ru/practicum/shareit/item/dto/CreateCommentDto.java
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/CreateCommentDto.java
@@ -4,11 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import ru.practicum.shareit.validationmarkers.Create;
+
+import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateCommentDto {
+    @NotBlank(groups = Create.class)
     private String text;
 }
